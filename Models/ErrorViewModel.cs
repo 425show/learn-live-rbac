1dc9eaa39b443decf8c89b9f70a38bf7decb8903
jpda <john.dandison@live.com>
2021-07-19 14:37:55 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace learn_live_rbac.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
